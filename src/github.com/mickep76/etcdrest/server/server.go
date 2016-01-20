db0e842f6cdbabbf8349084a9bb894fe36fbc4f4
Michael Persson <michael.ake.persson@gmail.com>
2016-01-20 16:54:39 +0100

    Add JSON patchH

--- src/github.com/mickep76/etcdrest/server/server.go
+++ src/github.com/mickep76/etcdrest/server/server.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/coreos/etcd/client"
+	"github.com/evanphx/json-patch"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mickep76/etcdmap"
@@ -61,7 +62,7 @@ func Create(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w
 	}
 }
 
-func Read(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w http.ResponseWriter, r *http.Request) {
+func Get(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := route.Path
 		name := mux.Vars(r)["name"]
@@ -92,8 +93,6 @@ func Update(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w
 		name := mux.Vars(r)["name"]
 		path := route.Path + "/" + name
 
-		// Get prev. values
-
 		body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 		if err != nil {
 			panic(err)
@@ -103,6 +102,71 @@ func Update(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w
 		}
 
 		docLoader := gojsonschema.NewStringLoader(string(body))
+		schemaLoader := gojsonschema.NewReferenceLoader(route.Schema)
+
+		result, err := gojsonschema.Validate(schemaLoader, docLoader)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+
+		if !result.Valid() {
+			var errors []string
+			for _, e := range result.Errors() {
+				errors = append(errors, fmt.Sprintf("%s: %s", strings.Replace(e.Context().String("/"), "(root)", path, 1), e.Description()))
+			}
+
+			writeErrors(cfg, w, r, errors, http.StatusBadRequest)
+			return
+		}
+
+		if err = etcdmap.CreateJSON(kapi, path, body); err != nil {
+			writeError(cfg, w, r, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
+	}
+}
+
+func Patch(cfg *config.Config, route *config.Route, kapi client.KeysAPI) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := mux.Vars(r)["name"]
+		path := route.Path + "/" + name
+
+		body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+		if err != nil {
+			panic(err)
+		}
+		if err := r.Body.Close(); err != nil {
+			panic(err)
+		}
+
+		res, err := kapi.Get(context.TODO(), path, &client.GetOptions{Recursive: true})
+		if err != nil {
+			// Path doesn't exist
+			if cerr, ok := err.(client.Error); ok && cerr.Code == 100 {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			// Error retrieving data
+			writeError(cfg, w, r, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		existData, err := etcdmap.JSON(res.Node)
+		if err != nil {
+			panic(err)
+		}
+
+		newData, err := jsonpatch.MergePatch(existData, body)
+		if err != nil {
+			panic(err)
+		}
+
+		docLoader := gojsonschema.NewStringLoader(string(newData))
 		schemaLoader := gojsonschema.NewReferenceLoader(route.Schema)
 
 		result, err := gojsonschema.Validate(schemaLoader, docLoader)
@@ -166,12 +230,14 @@ func Run(cfg *config.Config) {
 		log.Infof("Add endpoint: %s etcd path: %s", path, route.Path)
 		r.HandleFunc(path, Create(cfg, &route, kapi)).
 			Methods("POST")
-		r.HandleFunc(path, Read(cfg, &route, kapi)).
+		r.HandleFunc(path, Get(cfg, &route, kapi)).
 			Methods("GET")
-		r.HandleFunc(path+"/{name}", Read(cfg, &route, kapi)).
+		r.HandleFunc(path+"/{name}", Get(cfg, &route, kapi)).
 			Methods("GET")
 		r.HandleFunc(path+"/{name}", Update(cfg, &route, kapi)).
 			Methods("PUT")
+		r.HandleFunc(path+"/{name}", Update(cfg, &route, kapi)).
+			Methods("PATCH")
 		r.HandleFunc(path+"/{name}", Delete(cfg, &route, kapi)).
 			Methods("DELETE")
 	}
