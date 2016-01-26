f8c09f483d2654f764b4e936f8eb6f7e1cdac472
Michael Persson <michael.ake.persson@gmail.com>
2016-01-26 11:58:19 +0100

    No need for sub-commands

--- src/github.com/mickep76/etcdrest/main.go
+++ src/github.com/mickep76/etcdrest/main.go
@@ -4,9 +4,12 @@ import (
 	"os"
 	"time"
 
+	"github.com/bgentry/speakeasy"
 	"github.com/codegangsta/cli"
 
-	"github.com/mickep76/etcdrest/command"
+	"github.com/mickep76/etcdrest/etcd"
+	"github.com/mickep76/etcdrest/log"
+	"github.com/mickep76/etcdrest/server"
 )
 
 func main() {
@@ -26,11 +29,57 @@ func main() {
 		cli.DurationFlag{Name: "command-timeout, T", Value: 5 * time.Second, Usage: "Command timeout"},
 		cli.StringFlag{Name: "bind, b", Value: "0.0.0.0:8080", EnvVar: "ETCDREST_BIND", Usage: "Bind address"},
 		cli.StringFlag{Name: "api-version, V", Value: "v1", EnvVar: "ETCDREST_API_VERSION", Usage: "API Version"},
+		cli.StringFlag{Name: "print-config, P", Usage: "Print config"},
 	}
-	app.Commands = []cli.Command{
-		command.NewServerCmd(),
-		command.NewPrintConfigCmd(),
+	app.Action = func(c *cli.Context) {
+		runServer(c)
 	}
 
 	app.Run(os.Args)
+}
+
+func runServer(c *cli.Context) {
+	if c.GlobalBool("debug") {
+		log.SetDebug()
+	}
+
+	// Create etcd config.
+	ec := etcd.New()
+	ec.Peers(c.GlobalString("peers"))
+	ec.Cert(c.GlobalString("cert"))
+	ec.Key(c.GlobalString("key"))
+	ec.CA(c.GlobalString("ca"))
+	ec.Timeout(c.GlobalDuration("timeout"))
+	ec.CmdTimeout(c.GlobalDuration("command-timeout"))
+
+	// If user is set ask for password.
+	if c.GlobalString("user") != "" {
+		ec.User(c.GlobalString("user"))
+		pass, err := speakeasy.Ask("Password: ")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		ec.Pass(pass)
+	}
+
+	// Connect to etcd server.
+	es, err := ec.Connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Create server config.
+	sc := server.New(es)
+	sc.Bind(c.GlobalString("bind"))
+	sc.APIVersion(c.GlobalString("api-version"))
+	sc.Envelope(c.GlobalBool("envelope"))
+	//  sc.Indent(c.GlobalBool("indent"))
+
+	sc.RouteEtcd("/api/hosts", "/hosts", "file://schemas/host.json")
+	sc.RouteTemplate("/hosts/{name}", "host")
+
+	// Start server.
+	if err := sc.Run(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
